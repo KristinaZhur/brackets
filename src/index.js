67a69106f61301f5adda44a5c6968021012c12e6
Kristina Zhuravleva <krist-y_92@mail.ru>
2020-10-16 13:45:32 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    let checkStr =  bracketsConfig[i].join('');
+    console.log(checkStr)
+
+    if (str.includes(checkStr)){
+      str = str.replace(checkStr, '');
+      i =-1;
+    }
+  }
+  return str ? false : true;
 }
